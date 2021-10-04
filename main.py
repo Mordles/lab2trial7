67824235aa63f68b8b3af2ce1b9c2493ce4ab45c
Mordles <91845645+Mordles@users.noreply.github.com>
2021-10-04 14:26:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import RPi.GPIO as gpio
 from time import sleep
 
 gpio.setmode(gpio.BOARD)
-gpio.setup(5, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(26, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 def on_pushdown(channel):
     print ("Button Pushed.")
